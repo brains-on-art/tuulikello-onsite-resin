d40702c589e3523880c21afa9b2301c94a00b8e1
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-08-29 15:11:26 +0300

    maybe try to do it with bash and curl?

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 modprobe v4l2_common && python demo.py &
 cd /data
-python -m SimpleHTTPServer 80
+#python -m SimpleHTTPServer 80
+inotifywait -r -m -e modify ./data/image.jpg |
+   while read path _ file; do
+       echo $path$file modified
+       #curl -X POST -F 'image=@./data/image.jpg' http://ec2-52-58-239-33.eu-central-1.compute.amazonaws.com/lattice
+   done
