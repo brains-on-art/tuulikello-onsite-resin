89d5cd6513d7d83ab3f59966323e65f68947ccf6
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-08-29 14:53:19 +0300

    change everything

---
+++ demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+import time
+import picamera
+import requests
+
+with picamera.PiCamera() as camera:
+    # Camera warm-up time
+    time.sleep(1)
+    camera.resolution = (1920, 1080)
+    while True:
+        camera.capture('/data/image.jpg')
+
+        try:
+            files = {'image': open('/data/image.jpg', 'rb')}
+            r = requests.post(os.environ['REMOTE_URL'], files=files)
+        time.sleep(3)
